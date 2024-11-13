e6f7dd38b71d545f289e098fc35eff0252806c01
Matti Niinimäki <m@mansteri.com>
2024-11-14 00:00:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,20 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+   // When this user emits, client side: socket.emit('otherevent',some data);
+  socket.on('mouse',
+    function(data) {
+      // Data comes in as whatever was sent, including objects
+      console.log("Received: 'mouse' " + data.x + " " + data.y);
+      
+      // Send it to all other clients
+      socket.broadcast.emit('mouse', data);
+        
+      // This is a way to send to everyone including sender
+      // io.sockets.emit('message', "this goes to everyone");
+
+      }
+    );
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
